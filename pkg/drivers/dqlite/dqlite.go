0ebbf1160895530550bfa7b79ed3ac4b85b6d530
Louise K. Schmidtgen <louise.schmidtgen@canonical.com>
2024-08-29 15:39:09 +0200

    Configurable watch query timeout (Backport Microk8s v1.28) (#33)

--- pkg/drivers/dqlite/dqlite.go
+++ pkg/drivers/dqlite/dqlite.go
@@ -46,8 +46,9 @@ type opts struct {
 	dsn        string
 	driverName string // If not empty, use a pre-registered dqlite driver
 
-	compactInterval time.Duration
-	pollInterval    time.Duration
+	compactInterval   time.Duration
+	pollInterval      time.Duration
+	watchQueryTimeout time.Duration
 }
 
 func AddPeers(ctx context.Context, nodeStore client.NodeStore, additionalPeers ...client.NodeInfo) error {
@@ -164,6 +165,7 @@ func New(ctx context.Context, datasourceName string, tlsInfo tls.Config) (server
 
 	generic.CompactInterval = opts.compactInterval
 	generic.PollInterval = opts.pollInterval
+	generic.WatchQueryTimeout = opts.watchQueryTimeout
 	return backend, nil
 }
 
@@ -320,6 +322,12 @@ func parseOpts(dsn string) (opts, error) {
 			}
 			result.pollInterval = d
 			delete(values, k)
+		case "watch-query-timeout":
+			d, err := time.ParseDuration(vs[0])
+			if err != nil {
+				return opts{}, fmt.Errorf("failed to parse watch-query-timeout duration value %q: %w", vs[0], err)
+			}
+			result.watchQueryTimeout = d
 		}
 	}
 
--- pkg/logstructured/sqllog/sql.go
+++ pkg/logstructured/sqllog/sql.go
@@ -3,6 +3,7 @@ package sqllog
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -43,6 +44,7 @@ type Dialect interface {
 	IsFill(key string) bool
 	GetSize(ctx context.Context) (int64, error)
 	GetCompactInterval() time.Duration
+	GetWatchQueryTimeout() time.Duration
 	GetPollInterval() time.Duration
 }
 
@@ -384,10 +386,14 @@ func (s *SQLLog) poll(result chan interface{}, pollStart int64) {
 			}
 		}
 		waitForMore = true
+		watchCtx, cancel := context.WithTimeout(s.ctx, s.d.GetWatchQueryTimeout())
+		defer cancel()
 
-		rows, err := s.d.After(s.ctx, last, 500)
+		rows, err := s.d.After(watchCtx, last, 500)
 		if err != nil {
-			logrus.Errorf("fail to list latest changes: %v", err)
+			if !errors.Is(err, context.DeadlineExceeded) {
+				logrus.Errorf("fail to list latest changes: %v", err)
+			}
 			continue
 		}
 
